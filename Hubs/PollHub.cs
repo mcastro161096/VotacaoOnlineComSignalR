79dd68cc83973b724787d7469ddee2ed990b47ec
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-08-21 14:22:29 -0300

    Subindo projeto, já com Hub criado.

---
+++ Hubs/PollHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VotacaoOnlineComSignalR.Hubs
+{
+    public class PollHub : Hub
+    {
+        public async Task SendMessageAsync(string user, string message, string myChannelId, string myChannelVal)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message, myChannelId, myChannelVal).ConfigureAwait(false);
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VotacaoOnlineComSignalR.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
